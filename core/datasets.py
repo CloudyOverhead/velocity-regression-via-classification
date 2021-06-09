f67b28ef5e833404c32d3a65064488cacbe37a30
CloudyOverhead <jerome.simon@geolearn.ca>
2021-06-09 13:06:14 -0400

    Reintroduce datasets

--- core/datasets.py
+++ core/datasets.py
@@ -5,6 +5,9 @@ from os.path import abspath
 
 from GeoFlow.GeoDataset import GeoDataset
 from GeoFlow.DefinedDataset.Dataset2Dtest import Dataset2Dtest
+from GeoFlow.EarthModel import MarineModel
+from GeoFlow.SeismicGenerator import Acquisition
+from GeoFlow.GraphIO import Reftime, Vrms, Vint, Vdepth, ShotGather
 
 
 class Dataset(GeoDataset):
@@ -23,5 +26,93 @@ class Test2D(Dataset2Dtest):
         model, acquire, inputs, outputs = super().set_dataset()
         acquire.dg = 4
         acquire.ds = 16
+        return model, acquire, inputs, outputs
+
+
+class Article1D(Dataset):
+    def set_dataset(self):
+        self.trainsize = 5000
+        self.validatesize = 0
+        self.testsize = 10
+
+        model = MarineModel()
+        model.dh = 6.25
+        model.NX = 692 * 2
+        model.NZ = 752 * 2
+        model.layer_num_min = 48
+        model.layer_dh_min = 20
+        model.layer_dh_max = 50
+        model.water_vmin = 1430
+        model.water_vmax = 1560
+        model.water_dmin = .9 * model.water_vmin
+        model.water_dmax = 3.1 * model.water_vmax
+        model.vp_min = 1300.0
+        model.vp_max = 4000.0
+
+        acquire = Acquisition(model=model)
+        acquire.dt = .0004
+        acquire.NT = int(8 / acquire.dt)
+        acquire.resampling = 10
+        acquire.dg = 8
+        acquire.ds = 8
+        acquire.gmin = int(470 / model.dh)
+        acquire.gmax = int((470+72*acquire.dg*model.dh) / model.dh)
+        acquire.minoffset = 470
+        acquire.peak_freq = 26
+        acquire.df = 5
+        acquire.wavefuns = [0, 1]
+        acquire.source_depth = (acquire.Npad+4) * model.dh
+        acquire.receiver_depth = (acquire.Npad+4) * model.dh
+        acquire.tdelay = 3.0 / (acquire.peak_freq-acquire.df)
+        acquire.singleshot = True
+        acquire.configuration = 'inline'
+
+        inputs = {ShotGather.name: ShotGather(model=model, acquire=acquire)}
+        outputs = {Reftime.name: Reftime(model=model, acquire=acquire),
+                   Vrms.name: Vrms(model=model, acquire=acquire),
+                   Vint.name: Vint(model=model, acquire=acquire),
+                   Vdepth.name: Vdepth(model=model, acquire=acquire)}
+
+        for input in inputs.values():
+            input.train_on_shots = True  # 1D shots are CMPs.
+            input.mute_dir = True
+        for output in outputs.values():
+            output.train_on_shots = True
+            output.identify_direct = False
+
+        return model, acquire, inputs, outputs
+
+    def __init__(self, noise=False):
+        super().__init__()
+        if noise:
+            for input in self.inputs.values():
+                input.random_static = True
+                input.random_static_max = 1
+                input.random_noise = True
+                input.random_noise_max = 0.02
+                input.random_time_scaling = True
+
+
+class Article2D(Article1D):
+    def set_dataset(self):
+        model, acquire, inputs, outputs = super().set_dataset()
+
+        self.trainsize = 500
+        self.validatesize = 0
+        self.testsize = 100
+
+        model.max_deform_freq = .06
+        model.min_deform_freq = .0001
+        model.amp_max = 8
+        model.max_deform_nfreq = 40
+        model.prob_deform_change = .7
+        model.dip_max = 10
+        model.ddip_max = 4
+
+        acquire.singleshot = False
+        for input in inputs.values():
+            input.train_on_shots = False
+        for output in outputs.values():
+            output.train_on_shots = False
         return model, acquire, inputs, outputs
 
