aefedfade11754b6d1d3ff161864d6ce13a452e8
CloudyOverhead <jerome.simon@geolearn.ca>
2022-01-11 12:25:44 -0500

    Add a `list` argument for debugging figures

--- vmbrc/postprocess/__main__.py
+++ vmbrc/postprocess/__main__.py
@@ -6,21 +6,25 @@ from vmbrc.postprocess.catalog import catalog
 
 
 parser = ArgumentParser()
+parser.add_argument('--list', '-l', action='store_true')
 parser.add_argument('--metadata', '-m', action='store_true')
 parser.add_argument('--figure', '-f', type=str)
 parser.add_argument('--show', '-s', action='store_true')
 parser.add_argument('--gpus', type=int_or_list)
 args = parser.parse_args()
 
-if args.metadata:
-    if args.figure:
-        catalog.regenerate(args.figure, args.gpus)
-    else:
-        catalog.regenerate_all(args.gpus)
-
-if args.figure:
-    figure = catalog[args.figure]
-    figure.generate(args.gpus)
-    figure.save(show=args.show)
+if args.list:
+    print(catalog)
 else:
-    catalog.draw_all(args.gpus, show=args.show)
+    if args.metadata:
+        if args.figure:
+            catalog.regenerate(args.figure, args.gpus)
+        else:
+            catalog.regenerate_all(args.gpus)
+
+    if args.figure:
+        figure = catalog[args.figure]
+        figure.generate(args.gpus)
+        figure.save(show=args.show)
+    else:
+        catalog.draw_all(args.gpus, show=args.show)
