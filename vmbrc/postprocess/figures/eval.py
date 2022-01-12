2cdc0c2c691b5777869c6299f9d72db0f57287fb
CloudyOverhead <jerome.simon@geolearn.ca>
2022-01-12 15:31:04 -0500

    Compute metadata for standardized figure

--- vmbrc/postprocess/figures/eval.py
+++ vmbrc/postprocess/figures/eval.py
@@ -304,7 +304,7 @@ if __name__ == '__main__':
         dataset=args.dataset,
     )
 
-    # Eval.Metadata(args.gpus).generate(args.gpus)
+    Eval.Metadata(args.gpus).generate(args.gpus)
     figure = Eval()
     figure.generate(args.gpus)
     figure.save()
